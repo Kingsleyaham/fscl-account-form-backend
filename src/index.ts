9289db79efd821b87f9dd15b727602739b29dc92
Aham Kingsley <kingsleyaham6@gmail.com>
2023-04-24 18:27:22 +0100

    dommented out sequelize alter

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.listen(appConfig.PORT, async () => {
 
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ alter: true });
+    // await sequelize.sync({ alter: true });
     console.log("Connection to database successful.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
