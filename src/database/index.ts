f36eb7b375407718a56ac0b49a9c3b4cd4e9daa8
Aham Kingsley <kingsleyaham6@gmail.com>
2023-04-24 16:43:26 +0100

    updated env file

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,10 @@
 import { Sequelize } from "sequelize";
 import { dbConfig } from "../config";
 
-const sequelize = new Sequelize(
-  dbConfig.DB_NAME,
-  dbConfig.DB_USERNAME,
-  dbConfig.DB_PASSWORD,
-  {
-    host: dbConfig.DB_HOST,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize(dbConfig.DB_NAME, dbConfig.DB_USERNAME, dbConfig.DB_PASSWORD, {
+  host: dbConfig.DB_HOST,
+  dialect: "mysql",
+  port: parseInt(process.env.DB_PORT!) || 3306,
+});
 
 export default sequelize;
