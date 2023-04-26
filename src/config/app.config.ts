a3cb06a0d31a2e81c6d8f0d94d41f03718bcd8b9
Aham Kingsley <kingsleyaham6@gmail.com>
2023-04-26 10:50:35 +0100

    fix: amended joint account

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 const appConfig = {
   PORT: process.env.PORT! || 5000,
   HOST: process.env.HOST! || "localhost",
+  NODE_ENV: process.env.NODE_ENV!,
 };
 
 export default appConfig;
--- src/interfaces/jointAccount.interface.ts
+++ src/interfaces/jointAccount.interface.ts
@@ -3,14 +3,14 @@ import { IBankDetails } from "./bankDetails.interface";
 import { IContactDetails } from "./contactDetails.interface";
 import { IEmploymentDetails } from "./employmentDetails.interface";
 import { IInvestmentDetails } from "./investmentDetails.interface";
+import { IJointPersAcccount } from "./jointPersDetails.interface";
 import { IKycDocuments } from "./kyc.interface";
 import { INextOfKin } from "./nok.interface";
 import { IPepStatus } from "./pepStatus.interface";
-import { IPersonalDetails } from "./personalDetails.interface";
 import { ISignatories } from "./signatory.interface";
 
 export interface IJointAccount
-  extends IPersonalDetails,
+  extends IJointPersAcccount,
     IContactDetails,
     IBankDetails,
     IEmploymentDetails,
